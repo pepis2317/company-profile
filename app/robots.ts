56ba14f29556375ad78abc2a274d0582662f3982
pepis2317 <candradenis2317@gmail.com>
2026-07-30 10:58:11 +0700

    altered robots.ts and sitemap.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,6 @@ export default function robots() {
             userAgent: '*',
             allow: '/',
         },
-        sitemap: 'https://yoursite.com/sitemap.xml',
+        sitemap: 'https://mitra-kaltim-company-profile.vercel.app//sitemap.xml',
     };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://yoursite.com',
+            url: 'https://mitra-kaltim-company-profile.vercel.app',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1.0,
